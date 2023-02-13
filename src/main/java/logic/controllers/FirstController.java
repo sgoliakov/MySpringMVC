e3278914dc1f4d8d19ccaa6a31cc225c19c74953
Sviatoslav <sviatoslav.goliakov@gmail.com>
2023-02-13 10:38:39 +0200

    принимаем и передаем параметры в views

--- src/main/java/logic/controllers/FirstController.java
+++ src/main/java/logic/controllers/FirstController.java
@@ -1,18 +1,32 @@
 package logic.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 //@RequestMapping("/myFirstController") //поменяет url для запросов на контроллер
 public class FirstController {
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request, Model model) {//передаем запрос 1 способ
+        String name = request.getParameter("name");
+        int age = Integer.parseInt(request.getParameter("age"));
+//назначаем атрибут
+        model.addAttribute("message", "name: "+name+", "+"age: "+age);
+//не забываем обратиться к атрибуту в представлении
         return "first/helloPage";
     }
 
-    @GetMapping("goodbye")
-    public String goodBay() {
+    @GetMapping("goodbye")//передаем запрос 2способ
+    public String goodBay(@RequestParam(value = "name", required = false) String name,
+                          @RequestParam(value = "age", required = false) Integer age) {
+        System.out.println(name);
+        System.out.println(age);
+
+
+
         return "first/goodbyePage";
     }
 }
